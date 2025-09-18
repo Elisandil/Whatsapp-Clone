489315ca50f7a767af021fa803bfefdfde3296c8
aog-developer <elisandil88@gmail.com>
2025-09-18 07:14:57 +0200

    feat: Add auditing entity

---
+++ whatsappclone/src/main/java/com/whatsappclone/entities/BaseAuditingEntity.java
@@ -0,0 +1,34 @@
+package com.whatsappclone.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditingEntity {
+
+    @CreatedDate
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at", insertable = false)
+    private LocalDateTime lastModifiedAt;
+
+}
